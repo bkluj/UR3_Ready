40195d5752216af088e6b8a44ee83ccfce6fe4d5
Bartek <bartoszkluj@gmail.com>
2025-03-08 19:30:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import mujoco
+import mujoco.viewer
+
+# Wczytanie model UR3
+model = mujoco.MjModel.from_xml_path("scene.xml")
+data = mujoco.MjData(model)
+
+# Podgląd UR3
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
